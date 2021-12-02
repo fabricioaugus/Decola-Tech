5a5f84965aff4a5c63104476a00b6600ec3bc1f1
fabricioaugus <49557503+fabricioaugus@users.noreply.github.com>
2021-12-02 16:54:39 -0300

    Resolvido Desafio Quadrado de Pares

---
+++ Desafio C#/Desafio/Desafio/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio
+{
+    class Class1
+    {
+    }
+}
---
+++ Desafio C#/Desafio/Desafio/QuadradoDePares.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Desafio
+{/*
+        Leia um valor inteiro N. Apresente o quadrado de cada um dos valores pares, de 1 até N,
+        inclusive N, se for o caso.
+
+        Entrada
+        A entrada contém um valor inteiro N (5 < N < 2000).
+
+        Saída
+        Imprima o quadrado de cada um dos valores pares, de 1 até N, conforme o exemplo abaixo.
+
+        Tome cuidado! Algumas linguagens tem por padrão apresentarem como 
+        saída 1e+006 ao invés de 1000000 o que ocasionará resposta errada.
+        Neste caso, configure a precisão adequadamente para que isso não ocorra.
+  */
+    // Quadrado de Pares
+    class QuadradoDePares
+    {
+        static void Main(string[] args)
+        {
+            int value = 2;
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n; i += 2)
+            {
+                Console.WriteLine($"{i}^{value} = {(long)Math.Pow(i, value):N0}");
+            }
+        }
+    }
+}
