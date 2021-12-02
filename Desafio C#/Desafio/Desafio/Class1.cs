55f71b767af9621273b0a329b2e10e40a1f5d60d
fabricioaugus <49557503+fabricioaugus@users.noreply.github.com>
2021-12-02 16:57:51 -0300

    Resolvido Desafio Quadrado de Pares

--- Desafio C#/Desafio/Desafio/Class1.cs
+++ Desafio C#/Desafio/Desafio/Class1.cs
@@ -5,6 +5,14 @@ using System.Text;
 namespace Desafio
 {
     class Class1
+     { 
+        int inteiro = int.Parse(Console.ReadLine());
+        int inteiro2 = int.Parse(Console.ReadLine());
+        int sub = inteiro2 - inteiro - 1;
+        Console.WriteLine(sub);
+    }
+
+    internal class sub
     {
     }
 }
