2970411d33fdb43eeb017e119f2caa82268ad297
fabricioaugus <49557503+fabricioaugus@users.noreply.github.com>
2021-12-13 14:51:37 -0300

    ... class Calculadora

--- Programando com Orientação a Objeto com C#/ExemploPOO/Program.cs
+++ Programando com Orientação a Objeto com C#/ExemploPOO/Program.cs
@@ -7,6 +7,10 @@ namespace ExemploPOO
     {
         static void Main(string[] args)
         { 
+            // Calculadora class
+            Calculadora calculadora = new Calculadora(); 
+            Console.WriteLine($"Primeiro Somar: {calculadora.Somar(1,2)}");
+            Console.WriteLine($"Segundo Somar: {calculadora.Somar(1,2,6)}");
             // valores positivos validos
             //Retangulo r = new Retangulo();
             //r.DefinirMedida(30,30);
@@ -17,18 +21,18 @@ namespace ExemploPOO
             //Console.WriteLine($"Area: {r1.ObterArea()}");
            
            // ALUNO 
-            Aluno a1 = new Aluno();
-            a1.Nome = "José";
-            a1.Idade = 20;
-            a1.Nota = 10;
-            a1.Apresentar();
+            // Aluno a1 = new Aluno();
+            // a1.Nome = "José";
+            // a1.Idade = 20;
+            // a1.Nota = 10;
+            // a1.Apresentar();
 
-            // PROFESSOR
-            Professor p1= new Professor();
-            p1.Nome = "Marcos";
-            p1.Idade = 20;
-            p1.salario = 1100.23;
-            p1.Apresentar();
+            // // PROFESSOR
+            // Professor p1= new Professor();
+            // p1.Nome = "Marcos";
+            // p1.Idade = 20;
+            // p1.salario = 1100.23;
+            // p1.Apresentar();
 
            
         }
